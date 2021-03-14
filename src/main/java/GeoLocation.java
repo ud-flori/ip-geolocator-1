697e0ef5a2fb9ed297758d1f9a6d7be70393fbdd
Zselenák Flórián <flori.zs14@gmail.com>
2021-03-14 14:00:42 +0100

    Added GeoLocation record class

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,17 @@
+package GeoLocation;
+
+public record GeoLocation(
+        String as,
+        String city,
+        String country,
+        String countryCode,
+        String isp,
+        double lat,
+        double lon,
+        String org,
+        String query,
+        String region,
+        String regionName,
+        String timezone,
+        String zip
+        ){}
